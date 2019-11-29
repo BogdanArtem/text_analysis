795691effa73312e2732a97ae28c5a701a5a2ab5
BogdanArtem <50212691+BogdanArtem@users.noreply.github.com>
2019-11-29 12:23:30 +0200

    Add files via upload

--- text_analysis.py
+++ text_analysis.py
@@ -12,50 +12,104 @@
 # файле.
 
 import os, pymorphy2
+import re
 
-def analyse(file):
-#Perform text analysis
-#Returns dictionary
+ukrainian_letters = ['й','ц','у','к','е','н','г','ш','щ',
+	'з','х','ї','ґ','є','ж','д','л','о','р','п','а','в','i',
+	'ф','я','ч','с','м','и','т','ь','б','ю',]
 
-	ukrainian_letters = ['й','ц','у','к','е','н','г','ш','щ',
-	'з','х','ї','ґ','є','ж','д','л','о','р','п','а','в','і',
-	'ф','я','ч','с','м','и','т','ь','б','ю']
+class Text(object):
+	"""docstring for Text"""
+	def __init__(self, abs_path):
+		"""At initialization creates list of ukraninian words and list of sentences. Takes absolute path to the file as an argument"""
+		self.path = abs_path
 
-	#Split file into sentences
-	sentences = file.split('.')
+		#Set current dirrectory
+		os.curdir = self.path
 
-	#Split file into words
-	all_words = []
-	for sentence in sentences:
-		words = sentence.split(' ')
-		all_words.extend(words)
+		def is_ukr_word(word):
+		#Check if the word is ukrainian
+		#Returns Bool value
+			letters = list(word)
+			for letter in letters:
+				if letter.lower() in ukrainian_letters:
+					return True
+				else:
+					pass
+			return False
 
-	def is_ukr_word(word):
-	#Check if the word is ukrainian
-	#Returns Bool value
+		def clean_word(word):
+		#Remove all symbols except ukr letters
+		#Returns string
+			letters = list(word.strip())
+			for letter in letters:
+				if letter.lower() in ukrainian_letters:
+					letter = letter.lower()
+				else:
+					letters.remove(letter)
+			return "".join(letters)
 
-		letters = list(word)
-		for letter in letters:
-			if letter.lower() in ukrainian_letters:
-				return True
-			else:
-				pass
-		return False
 
-	def clean_word(word):
-	#Remove all symbols except ukr letters
-	#Returns string
+		# Open file for reading
+		with open(self.path, mode='r', encoding='utf-8') as f:
+			file_content = f.read()
 
-		letters = list(word)
-		for letter in letters:
-			if letter.lower() in ukrainian_letters:
-				letter = letter.lower()
-			else:
-				letters.remove(letter)
-		return "".join(letters)
+			#Split file into sentences
 
-	def frequency_dict(list_of_words):
-	#Dictionary of words frequency
+			sentences = re.split('\.|\n',file_content)
+			self.sentences = [sentence for sentence in sentences if sentence != '']
+
+			#Split file into words
+			all_words = []
+			for sentence in self.sentences:
+				words = sentence.split(' ')
+				all_words.extend(words)
+
+			# Creates a new list of cleaned words
+			self.ukr_words = []
+			for word in all_words:
+				if is_ukr_word(word):
+					self.ukr_words.append(clean_word(word))
+
+
+		# Create pymorphy2 instance for methods that use pymorphy2
+		self.morph = pymorphy2.MorphAnalyzer(lang='uk')
+
+
+	def analyse(self):
+		"""Returns number of words, number of sentences, average word length and average sentence length in tuple """
+
+		#Num of words
+		num_words = len(self.ukr_words)
+
+		#Num of sentences
+		num_sent = len(self.sentences)
+
+		#Average length of word
+		total_length = 0
+		for word in self.ukr_words:
+			total_length += len(word)
+		av_word_len = total_length/num_words
+
+		#Average length of sentence
+		total_length = 0
+		for sentence in self.sentences:
+			total_length += len(sentence)
+		av_sent_len = total_length/num_words
+
+		#Save num_words for get_parts_of_speach_freq()
+		self.num_words = num_words
+
+		return num_words, num_sent, av_word_len, av_sent_len
+
+
+	def freq_dict(self, list_of_words = None):
+		"""Returns key value pair of all ukrainian words and relative frequency if no other list is given
+		{key(str):value(float)}
+		"""
+		if not list_of_words:
+			list_of_words = self.ukr_words
+
 		unique_words = set(list_of_words)
 		num_of_un_words = len(unique_words)
 		freq_dict = {}
@@ -70,113 +124,64 @@ def analyse(file):
 			new_freq_dict[word] = (freq_dict[word]/num_of_un_words)*100
 		return new_freq_dict
 
-	# Creates a new list of cleaned words
-	new_words = []
-	for word in all_words:
-		if is_ukr_word(word):
-			new_words.append(clean_word(word))
 
-	#Num of words
-	num_words = len(new_words)
-
-	#Num of sentences
-	num_sent = len(sentences)
-
-	#Average length of word
-	total_length = 0
-	for new_word in new_words:
-		total_length += len(new_word)
-	av_word_len = total_length/num_words
-
-	#Average length of sentence
-	total_length = 0
-	for sentence in sentences:
-		total_length += len(sentence)
-	av_sent_len = total_length/num_words
-	
-	words_dict = frequency_dict(new_words)
-
-	#Dictionary of words frequency using lemmas
-	morph = pymorphy2.MorphAnalyzer(lang='uk')
-
-	lemmas = []
-	for new_word in new_words:
-		p = morph.parse(new_word)[0]
-		lemmas.append(p.normal_form)
-
-	lemmas_dict = frequency_dict(lemmas)
-
-	parts_of_speech = {}
-	for word in new_words:
-		p = morph.parse(word)[0]
-
-		if p.tag.POS in parts_of_speech:
-			parts_of_speech[p.tag.POS] += 1
-		else:
-			parts_of_speech[p.tag.POS] = 0
-
-	for keyvalue in parts_of_speech:
-		parts_of_speech[keyvalue] = (parts_of_speech[keyvalue]/num_words)*100
-
-	print(f'''
-		Number of words: {num_words}
-		Number of sentences: {num_sent}
-		Average word length: {av_word_len}
-		Average sentence length: {av_sent_len}
-		====================================================
-		====================================================
-		====================================================
-
-		     Relative frequency of words 
-
-		====================================================
-		====================================================
-		====================================================
-
-		{words_dict}
-
-		====================================================
-		====================================================
-		====================================================
-
-		     Relative frequency of words using lemmas 
-
-		====================================================
-		====================================================
-		====================================================
-
-		{lemmas_dict}
-
-		====================================================
-		====================================================
-		====================================================
-
-		     Relative frequency of parts of speach 
-
-		====================================================
-		====================================================
-		====================================================
-
-		{parts_of_speech}
-	''')
+	def get_relative_lemma_freq(self):
+		"""Returns key value pair of lemmas and relative frequency
+		{key(str):value(float)}
+		"""
+		lemmas = []
+		for word in self.ukr_words:
+			p = self.morph.parse(word)[0]
+			lemmas.append(p.normal_form)
 
 
-def main():
-# Load files from your directory and analyse texts
+		lemmas_dict = self.freq_dict(list_of_words = lemmas)
+		return lemmas_dict
 
-	texts_path = input("Enter absolute path to folder with your texts > ")
-	os.curdir = texts_path
 
-	filenames = os.listdir(os.curdir)
-	for file in filenames:
-		filepath = os.path.join(os.curdir,file)
-		with open(filepath, mode='r', encoding='utf-8') as f:
-			file_content = f.read()
-			print(file)
-			analyse(file_content)
-			print('To analyse next text hit Enter')
-			input()
+	def get_parts_of_speach_freq(self):
+		"""Returns key value pair of parts of speach frequency"""
+		parts_of_speech = {}
+		for word in self.ukr_words:
+			p = self.morph.parse(word)[0]
+
+			if p.tag.POS in parts_of_speech:
+				parts_of_speech[p.tag.POS] += 1
+			else:
+				parts_of_speech[p.tag.POS] = 0
+
+		for keyvalue in parts_of_speech:
+			parts_of_speech[keyvalue] = (parts_of_speech[keyvalue]/self.num_words)*100
+		return parts_of_speech
+
 
 
 if __name__ == '__main__':
-	main()
+
+	path = input('Please, enter absolute path to your .txt file > ')
+	text = Text(path)
+
+	print((text.analyse()))
+	num_words, num_sent, av_word_len, av_sent_len = text.analyse()
+
+	print("="*100)
+	print(f"Number of words: {num_words}, number of sentences: {num_sent}, average word length: {av_word_len}, average sentence length {av_sent_len}")
+	print("="*100)
+
+	print(text.freq_dict())
+	print("="*100)
+
+	print(text.get_relative_lemma_freq())
+	print("="*100)
+
+	print(text.get_parts_of_speach_freq())
+	print("="*100)
+
+
+
+
+
+
+
+
+
